b17f482fd5ee9ae2c6fe091d3f3a97712715656f
dayo2n <0217dayun@naver.com>
2021-10-31 15:33:56 +0900

    upload leetcode : Permutations by Java

--- LeetCode/Permutations.java
+++ LeetCode/Permutations.java
@@ -1,4 +1,28 @@
 package LeetCode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Permutations {
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        backtracking(nums, result, new LinkedList<>());
+        return result;
+    }
+
+    public void backtracking(int[] nums, List<List<Integer>> result, List<Integer> list) {
+        if (list.size() == nums.length) {
+            result.add(new LinkedList<>(list));
+            return;
+        }
+
+        for (int num : nums) {
+            if (list.contains(num)) continue;
+
+            list.add(num);
+            backtracking(nums, result, list);
+            list.remove(list.size() - 1);
+        }
+    }
 }
