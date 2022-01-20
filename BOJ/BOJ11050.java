4aa7b05c1f075964f1d6a0d341b7f67b3a049b98
dayo2n <0217dayun@naver.com>
2022-01-20 18:34:48 +0900

    solve: with recursion \'factorial\'

---
+++ BOJ/BOJ11050.java
@@ -0,0 +1,16 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ11050 {
+    static int factorial(int n){
+        if(n == 0) return 1;
+        return n * factorial(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int L = sc.nextInt();
+        System.out.println(factorial(N) / (factorial(L) * factorial(N-L)));
+    }
+}
