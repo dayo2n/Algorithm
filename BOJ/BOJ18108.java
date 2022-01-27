e72e5dd29b8e794ae4c69a5eab28c78ddd066ba0
dayo2n <0217dayun@naver.com>
2022-01-27 13:34:56 +0900

    solve: math

---
+++ BOJ/BOJ18108.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+import java.io.*;
+public class BOJ18108 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N-543);
+    }
+}
