1b12d3ccd44229c20c54d35363d062679dc3752e
dayo2n <0217dayun@naver.com>
2022-01-30 17:14:54 +0900

    solve 3004: print

---
+++ BOJ/BOJ3004.java
@@ -0,0 +1,9 @@
+package BOJ;
+
+public class BOJ3004 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
