7d9032ebffcd23eb53cbf5af74e7e2f570fb42c0
dayo2n <0217dayun@naver.com>
2022-02-24 01:51:26 +0900

    solve 3273 : two pointer

---
+++ BOJ/BOJ3273.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ3273 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int [] numbers = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int n = 0; n < N; n++) numbers[n] = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(br.readLine());
+
+        Arrays.sort(numbers);
+
+        int cnt = 0;
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == x) {
+                cnt++;
+                left++;
+                right--;
+            }
+            else if (numbers[left] + numbers[right] > x) {
+                right--;
+            } else left++;
+        }
+        System.out.println(cnt);
+    }
+}
