ff1a8063ba42d2c8cd5547860569f3fa2cf23d50
dayo2n <0217dayun@naver.com>
2022-05-25 23:34:12 +0900

    [solve] Lv.2 가장 큰 수

---
+++ Swift/programmers/Programmers/Programmers/solved/Solution42746.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 문다 on 2022/05/21.
+//
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> String {
+    
+    let num : [Int] = numbers.sorted { Int("\($0)\($1)")! > Int("\($1)\($0)")! }
+    
+    // 첫번째 요소부터 0이라면 전부 0이므로
+    if num[0] == 0 {
+        return "0"
+    }
+    
+//    var answer : String = ""
+//
+//    for n in num {
+//        answer += String(n)
+//    }
+//
+//    return answer
+    
+    return num.reduce("") { $0 + "\($1)"}
+}
+
+print(solution([6, 10, 2]))
+print(solution([3, 30, 34, 5, 9]))
