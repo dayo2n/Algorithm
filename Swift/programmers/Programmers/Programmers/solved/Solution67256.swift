4c84e17f2c9541462a68b4b4de30dad85d0722f6
dayo2n <0217dayun@naver.com>
2022-06-26 23:49:47 +0900

    [unsolved & solve] Lv1. 카카오 인턴_키패드 누르기

---
+++ Swift/programmers/Programmers/Programmers/solved/Solution67256.swift
@@ -0,0 +1,60 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 문다 on 2022/05/21.
+//
+import Foundation
+
+func solution(_ numbers:[Int], _ hand:String) -> String {
+  enum KeypadHand {
+    case left
+    case right
+  }
+  
+  var result = ""
+  var leftHand = 10
+  var rightHand = 12
+  
+  for i in numbers {
+    let number = i == 0 ? 11 : i
+    var tapHand: KeypadHand = .left
+    
+    switch number {
+    case 1, 4, 7:
+      tapHand = .left
+      
+    case 3, 6, 9:
+      tapHand = .right
+      
+    case 2, 5, 8, 11:
+      let leftTemp = (number - leftHand).magnitude
+      let rightTemp = (number - rightHand).magnitude
+      
+      let leftDistance = (leftTemp / 3) + (leftTemp % 3)
+      let rightDistance = (rightTemp / 3) + (rightTemp % 3)
+      
+      if leftDistance == rightDistance {
+        tapHand = hand == "left" ? .left : .right
+      } else {  // 그 외
+        tapHand = leftDistance < rightDistance ? .left : .right
+      }
+      
+    default:
+      break
+    }
+    
+    if tapHand == .left {
+      result.append("L")
+      leftHand = number
+    } else {
+      result.append("R")
+      rightHand = number
+    }
+  }
+  
+  return result
+}
+print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"))
+print(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left"))
+print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right"))
---
+++ Swift/programmers/Programmers/Programmers/unsolved/Solution57256.swift
@@ -0,0 +1,64 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 문다 on 2022/05/21.
+//
+import Foundation
+
+func solution(_ numbers:[Int], _ hand:String) -> String {
+    
+    enum KeypadHand {
+        case left
+        case right
+      }
+    
+    var answer : String = ""
+    var currentLeft : Int = 10 // *
+    var currentRight : Int = 12 // #
+    
+    for number in numbers {
+        
+        let target = number == 0 ? 11 : number // 키패드 숫자가 0일 경우 11로 바꿔준다
+        var tapHand: KeypadHand = .left // 사용한 손을 저장할 변수 선언
+        
+        switch target {
+            
+        case 1, 4, 7:
+            tapHand = .left
+            
+        case 3, 6, 9:
+            tapHand = .right
+            
+        case 2, 5, 8, 11:
+            let leftTemp = (number - currentLeft).magnitude
+            let rightTemp = (number - currentRight).magnitude
+              
+          let leftDistance = (leftTemp / 3) + (leftTemp % 3)
+          let rightDistance = (rightTemp / 3) + (rightTemp % 3)
+          
+          if leftDistance == rightDistance {
+            tapHand = hand == "left" ? .left : .right
+          } else {
+            tapHand = leftDistance < rightDistance ? .left : .right
+          }
+            
+        default:
+            break
+        }
+        
+        if tapHand == .left {
+          answer.append("L")
+          currentLeft = target
+        } else {
+          answer.append("R")
+          currentRight = target
+        }
+    }
+    
+    return answer
+}
+
+print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"))
+print(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left"))
+print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right"))
