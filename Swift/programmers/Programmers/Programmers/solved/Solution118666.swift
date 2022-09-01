430a1bd6693d6f9695e15228ffc73abdc297e4c1
dayo2n <0217dayun@naver.com>
2022-09-01 23:59:04 +0900

    [solve] Lv1. 서울에서 김서방 찾기

---
+++ Swift/programmers/Programmers/Programmers/solved/Solution118666.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 문다 on 2022/05/21.
+//
+
+import Foundation
+
+func solution(_ seoul:[String]) -> String {
+    return "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"
+}
+
+solution(["Jane", "Kim"])
+
