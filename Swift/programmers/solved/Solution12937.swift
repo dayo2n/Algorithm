5f246b8180adcdbe09a8196709010f3176880f45
dayo2n <0217dayun@naver.com>
2022-09-07 18:52:11 +0900

    [refactor] Lv1. 짝수와 홀수: 삼항연산 사용

--- Swift/programmers/solved/Solution12937.swift
+++ Swift/programmers/solved/Solution12937.swift
@@ -5,8 +5,9 @@
 //  Created by 문다 on 2022/05/21.
 //
 func solution(_ num:Int) -> String {
-    if num % 2 == 0 {
-        return "Even"
-    }
-    return "Odd"
+//    if num % 2 == 0 {
+//        return "Even"
+//    }
+//    return "Odd"
+    return num % 2 == 0 ? "Even" : "Odd"
 }
