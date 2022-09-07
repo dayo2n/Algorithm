058c128307f92f055fbbd213fd0d13712d40f293
dayo2n <0217dayun@naver.com>
2022-09-07 19:20:18 +0900

    [solve] Lv1. 평균구하기: reduce

---
+++ Swift/programmers/solved/Solution12944.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 문다 on 2022/05/21.
+//
+func solution(_ arr:[Int]) -> Double {
+    return Double(arr.reduce(0,+)) / Double(arr.count)
+    // ! runtime error : reduce(0, +) // space
+}
