eaf7410cd43123e7af865d77382d7ce481f25e47
dayo2n <0217dayun@naver.com>
2022-09-07 18:49:58 +0900

    [solve] Lv1. 짝수와 홀수

---
+++ Swift/programmers/solved/Solution12937.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 문다 on 2022/05/21.
+//
+func solution(_ num:Int) -> String {
+    if num % 2 == 0 {
+        return "Even"
+    }
+    return "Odd"
+}
