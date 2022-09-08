2f9dbfcd0984adb543a229a890e78fa051e36936
dayo2n <0217dayun@naver.com>
2022-09-08 23:34:37 +0900

    [refactor] Lv1. 핸드폰 번호 가리기: String(repeating, count), String.suffix)

--- Swift/programmers/solved/Solution12948.swift
+++ Swift/programmers/solved/Solution12948.swift
@@ -1,3 +1,4 @@
 func solution(_ phone_number:String) -> String {
-     return Array(repeating: "*", count: phone_number.count - 4).reduce("",+) + phone_number[phone_number.index(phone_number.startIndex, offsetBy: phone_number.count - 4)..<phone_number.endIndex]
+//     return Array(repeating: "*", count: phone_number.count - 4).reduce("",+) + phone_number[phone_number.index(phone_number.startIndex, offsetBy: phone_number.count - 4)..<phone_number.endIndex]
+    return "\(String(repeating: "*", count: phone_number.count - 4))\(phone_number.suffix(4))"
 }
