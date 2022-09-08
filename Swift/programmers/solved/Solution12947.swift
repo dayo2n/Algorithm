5814a4f880bbfdeafcdd5350e1f419e90cfdf878
dayo2n <0217dayun@naver.com>
2022-09-08 22:10:09 +0900

    [refactor] Lv1. 하샤드 수: 삼항연산 삭제

--- Swift/programmers/solved/Solution12947.swift
+++ Swift/programmers/solved/Solution12947.swift
@@ -5,5 +5,5 @@
 //  Created by 문다 on 2022/05/21.
 //
 func solution(_ x:Int) -> Bool {
-    return x % Array(String(x)).compactMap{Int(String($0))!}.reduce(0, +) == 0 ? true : false
+    return x % Array(String(x)).compactMap{Int(String($0))!}.reduce(0, +) == 0
 }
