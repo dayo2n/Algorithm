b48402542aa416523f532e5903b4d0ecaae229b8
dayo2n <0217dayun@naver.com>
2022-09-08 23:42:59 +0900

    [comment] 오답 코드를 주석으로 추가

--- Swift/programmers/solved/Solution12935.swift
+++ Swift/programmers/solved/Solution12935.swift
@@ -1,5 +1,6 @@
 func solution(_ arr:[Int]) -> [Int] {
     if arr.count <= 1 { return [-1] }
     let lowest = arr.min()
-    return arr.filter{$0 != lowest} // 한 tc가 시간초과
+    return arr.filter{$0 != lowest} // 실행속도 훨씬 빠름
+//    return arr.filter{$0 != arr.min()} // 한 tc가 시간초과
 }
