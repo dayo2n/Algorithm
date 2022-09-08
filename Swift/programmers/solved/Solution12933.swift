47011f4da1c98eb1aba151bf09f853ae423b86b9
dayo2n <0217dayun@naver.com>
2022-09-08 21:49:00 +0900

    [refactor] Lv1. 정수 내림차순으로 배치하기: 배열 변환 삭제

--- Swift/programmers/solved/Solution12933.swift
+++ Swift/programmers/solved/Solution12933.swift
@@ -5,6 +5,8 @@
 //  Created by 문다 on 2022/05/21.
 //
 func solution(_ n:Int64) -> Int64 {
-    let reversedArray = Array(String(n)).compactMap{Int(String($0))}.sorted(by: >)
-    return Int64(reversedArray.compactMap{String($0)}.reduce("", {return $0 + $1}))!
+//    let reversedArray = Array(String(n)).compactMap{Int(String($0))}.sorted(by: >)
+//    return Int64(reversedArray.compactMap{String($0)}.reduce("", {return $0 + $1}))!
+    return Int64(String(String(n).sorted(by: >)))!
+    // 정수형 배열로 변환하지않고 string 상태에서 정렬하면 된다 .......
 }
