d2dae6986460cadf222eb65bd52b873999b580b5
dayo2n <0217dayun@naver.com>
2022-09-08 23:51:16 +0900

    [refactor] Lv1. 나누어 떨어지는 숫자 배열: 삼항연산 이용"

--- Swift/programmers/solved/Solution12910.swift
+++ Swift/programmers/solved/Solution12910.swift
@@ -1,6 +1,9 @@
 func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
     
-    let quota = arr.filter{$0 % divisor == 0}
-    if quota.count == 0 { return [-1]}
-    return quota.sorted()
+//    let quota = arr.filter{$0 % divisor == 0}
+//    if quota.count == 0 { return [-1]}
+//    return quota.sorted()
+    
+    let result = arr.filter{$0 % divisor == 0}.sorted()
+    return result == [] ? [-1] : result
 }
