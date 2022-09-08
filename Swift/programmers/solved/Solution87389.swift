f8d37a31776b4d0cd26964d11bbc775a85973ce1
dayo2n <0217dayun@naver.com>
2022-09-08 19:55:12 +0900

    [solve] Lv1. 나머지가 1이 되는 수 찾기

---
+++ Swift/programmers/solved/Solution87389.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 문다 on 2022/05/21.
+//
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    
+    var x = 2
+    while n % x != 1 {
+        x = x + 1
+    }
+    return x
+}
