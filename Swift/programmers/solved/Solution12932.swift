2b2c1ce6d30c8fca4da31ee20f46afb673aba95d
dayo2n <0217dayun@naver.com>
2022-09-08 21:05:52 +0900

    [refactor] Lv1. 자연수 뒤집어 배열로 만들기: compactMap

--- Swift/programmers/solved/Solution12932.swift
+++ Swift/programmers/solved/Solution12932.swift
@@ -5,5 +5,6 @@
 //  Created by 문다 on 2022/05/21.
 //
 func solution(_ n:Int64) -> [Int] {
-    return Array(String(n).reversed()).map{ Int(String($0))! }
+//    return Array(String(n).reversed()).map{ Int(String($0))! }
+    return Array(String(n).reversed()).compactMap{ Int(String($0)) }
 }
