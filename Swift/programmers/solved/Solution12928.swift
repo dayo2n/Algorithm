5cc0e3f3cdc1962524a5c6dfd0c1b86c4d667f82
dayo2n <0217dayun@naver.com>
2022-09-08 20:30:58 +0900

    [refactor] Lv1. 약수의 합: guard, filter + reduce

--- Swift/programmers/solved/Solution12928.swift
+++ Swift/programmers/solved/Solution12928.swift
@@ -8,21 +8,25 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
+//
+//    if n == 0 {
+//        return 0
+//    } else if n == 1 {
+//        return 1
+//    }
+//
+//    var x: Int = 2
+//    var answer: Int = 1
+//
+//    while x <= n {
+//        if n % x == 0 {
+//            answer = answer + x
+//        }
+//        x = x + 1
+//    }
+//    return answer
     
-    if n == 0 {
-        return 0
-    } else if n == 1 {
-        return 1
-    }
     
-    var x: Int = 2
-    var answer: Int = 1
-    
-    while x <= n {
-        if n % x == 0 {
-            answer = answer + x
-        }
-        x = x + 1
-    }
-    return answer
+    guard n != 0 else { return 0 }
+    return Array(1...n).filter({n % $0 == 0}).reduce(0, +)
 }
