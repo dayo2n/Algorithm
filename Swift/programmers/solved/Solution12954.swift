f0c6381765301e04544fc5197b0cd3370ccccf54
dayo2n <0217dayun@naver.com>
2022-09-08 22:46:41 +0900

    [refactor] Lv1. x만큼 간격이 있는 n개의 숫자: 사칙연산, map

--- Swift/programmers/solved/Solution12954.swift
+++ Swift/programmers/solved/Solution12954.swift
@@ -5,9 +5,10 @@
 //  Created by 문다 on 2022/05/21.
 //
 func solution(_ x:Int, _ n:Int) -> [Int64] {
-    var answer: [Int64] = [Int64(x)]
-    for _ in (1..<n) {
-        answer.append(answer[answer.count - 1] + Int64(x))
-    }
-    return answer
+//    var answer: [Int64] = [Int64(x)]
+//    for _ in (1..<n) {
+//        answer.append(answer[answer.count - 1] + Int64(x))
+//    }
+//    return answer
+    return Array(1...n).map{Int64($0 * x)}
 }
