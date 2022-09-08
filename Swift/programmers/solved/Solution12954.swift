cbc5651f1b3d6531fdbce1d3c26af430526560a7
dayo2n <0217dayun@naver.com>
2022-09-08 22:44:54 +0900

    [solve] Lv1. x만큼 간격이 있는 n개의 숫자

---
+++ Swift/programmers/solved/Solution12954.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 문다 on 2022/05/21.
+//
+func solution(_ x:Int, _ n:Int) -> [Int64] {
+    var answer: [Int64] = [Int64(x)]
+    for _ in (1..<n) {
+        answer.append(answer[answer.count - 1] + Int64(x))
+    }
+    return answer
+}
