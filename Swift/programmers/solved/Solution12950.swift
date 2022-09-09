5539de4d0d2b58247764f3979e279aa639ba4ffd
dayo2n <0217dayun@naver.com>
2022-09-09 13:49:13 +0900

    [solve, refactor] Lv1. 행렬의 덧셈: for문, zip, map -> for문 삭제, zip, map 중첩 사용

---
+++ Swift/programmers/solved/Solution12950.swift
@@ -0,0 +1,9 @@
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+//    var answer : [[Int]] = []
+//
+//    for (idx, arr) in arr1.enumerated() {
+//        answer.append(zip(arr1[idx], arr2[idx]).map(+))
+//    }
+//    return answer
+    return zip(arr1, arr2).map{zip($0, $1).map(+)}
+}
