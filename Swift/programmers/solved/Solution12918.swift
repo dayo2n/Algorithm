8aa3fbe88b248a171790782c879fda30e96d6034
dayo2n <0217dayun@naver.com>
2022-09-09 13:22:09 +0900

    [refactor] Lv1. 문자열 다루기 기본: String -> Int 형변환시 정수로 변환될 수 없는 문자열이 껴있으면 nil을 반환하는 성질을 이용

--- Swift/programmers/solved/Solution12918.swift
+++ Swift/programmers/solved/Solution12918.swift
@@ -1,5 +1,6 @@
 func solution(_ s:String) -> Bool {
     guard s.count == 4 || s.count == 6 else { return false }
-    let numbers = Array(0...9).map{String($0)}
-    return s.filter{!numbers.contains(String($0))} == ""
+//    let numbers = Array(0...9).map{String($0)}
+//    return s.filter{!numbers.contains(String($0))} == ""
+    return Int(s) != nil
 }
