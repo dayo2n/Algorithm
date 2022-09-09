ddc6856fab6d2bd5d78b9b9f6ee72bcb83062048
dayo2n <0217dayun@naver.com>
2022-09-09 12:44:44 +0900

    [refactor] Lv1. 가운데 글자 가져오기: offset을 조정하고 범위를 이용해 삼항연산 삭제

--- Swift/programmers/solved/Solution12903.swift
+++ Swift/programmers/solved/Solution12903.swift
@@ -1,3 +1,4 @@
 func solution(_ s:String) -> String {
-    return s[s.index(s.startIndex, offsetBy: s.count / 2)] + (s.count % 2 == 0 ? String(s[s.index(s.startIndex, offsetBy: s.count / 2 + 1)]) : "")
+//    return s[s.index(s.startIndex, offsetBy: s.count / 2)] + (s.count % 2 == 0 ? String(s[s.index(s.startIndex, offsetBy: s.count / 2 + 1)]) : "")
+    return String(s[s.index(s.startIndex, offsetBy: (s.count - 1) / 2)...s.index(s.startIndex, offsetBy: s.count / 2)])
 }
