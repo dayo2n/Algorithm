8ae1f0058bfcb48ef66c7637155b0039099a1d29
dayo2n <0217dayun@naver.com>
2022-09-12 00:06:45 +0900

    [refactor] Lv1. 문자열 내 마음대로 정렬하기: Array(String)을 사용하면 대괄호[]만으로 인덱스 접근 가능

--- Swift/programmers/solved/Solution12915.swift
+++ Swift/programmers/solved/Solution12915.swift
@@ -1,8 +1,11 @@
 func solution(_ strings:[String], _ n:Int) -> [String] {
+//    return strings.sorted {
+//        var idx0 = $0.index($0.startIndex, offsetBy: n)
+//        var idx1 = $1.index($1.startIndex, offsetBy: n)
+//        if $0[idx0] == $1[idx1] { return $0 < $1 }
+//        return $0[idx0] < $1[idx1]
+//    }
     return strings.sorted {
-        var idx0 = $0.index($0.startIndex, offsetBy: n)
-        var idx1 = $1.index($1.startIndex, offsetBy: n)
-        if $0[idx0] == $1[idx1] { return $0 < $1 }
-        return $0[idx0] < $1[idx1]
+        return Array($0)[n] == Array($1)[n] ? $0 < $1 : Array($0)[n] < Array($1)[n]
     }
 }
