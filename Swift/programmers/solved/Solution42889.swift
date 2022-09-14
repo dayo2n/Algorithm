9ec4a0b27d50f70d6a49e387c4395a11c0ed659f
dayo2n <0217dayun@naver.com>
2022-09-14 16:17:28 +0900

    [solve] Lv1. 실패율: dictionary 대신 tuple 사용, filter -> for문으로 시간 단축

---
+++ Swift/programmers/solved/Solution42889.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var failure = [(Int, Double)]()
+    var user = stages.count
+    
+    for n in (1...N) {
+        // var challenger = stages.filter{$0==n}.count // 아직 클리어하지 못한 유저
+        var challenger = 0
+        for stage in 0..<stages.count {
+            if stages[stage] == n { challenger += 1 }
+        }
+
+        failure.append((n, Double(challenger) / Double(user)))
+        user -= challenger
+    }
+    return failure.sorted{$0.1 > $1.1}.map{$0.0}
+}
