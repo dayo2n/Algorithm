96ae416c440a197fefadc74bdfe457bc0c53c99a
dayo2n <0217dayun@naver.com>
2022-09-19 12:41:34 +0900

    [refactor] Lv2. 피보나치 수: dp

--- Swift/programmers/solved/Solution12945.swift
+++ Swift/programmers/solved/Solution12945.swift
@@ -1,11 +1,21 @@
+// 1. 반복문으로 풀기
+//func solution(_ n:Int) -> Int {
+//    var fibo1 = 0
+//    var fibo2 = 1
+//    var temp: Int
+//    for fibo in (2...n) {
+//        temp = fibo1 % 1234567
+//        fibo1 = fibo2 % 1234567
+//        fibo2 = temp + fibo1
+//    }
+//    return fibo2 % 1234567
+//}
+
+// 2. dp로 풀기
 func solution(_ n:Int) -> Int {
-    var fibo1 = 0
-    var fibo2 = 1
-    var temp: Int
+    var dp:[Int] = [0, 1]
     for fibo in (2...n) {
-        temp = fibo1 % 1234567
-        fibo1 = fibo2 % 1234567
-        fibo2 = temp + fibo1
+        dp.append((dp[fibo-1] + dp[fibo-2]) % 1234567)
     }
-    return fibo2 % 1234567
+    return dp[n]
 }
