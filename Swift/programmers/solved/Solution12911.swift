3e6b3e97638d285f08fa38238597a295f5e73981
dayo2n <0217dayun@naver.com>
2022-09-20 15:38:43 +0900

    [solve] Lv2. 다음 큰 숫자: 타입 추론 때문에 효율성 테스트에서 여러번 틀린 문제, 가능하면 변수 또는 상수의 타입을 명시하자

---
+++ Swift/programmers/solved/Solution12911.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int) -> Int
+{
+    var answer:Int = n + 1
+    let binaryString: String = String(n, radix: 2)
+    let countOfOne: Int = binaryString.filter{$0=="1"}.count
+    
+    while(true) {
+        if String(answer, radix: 2).filter{$0=="1"}.count == countOfOne { break }
+        answer += 1
+    }
+    return answer
+}
