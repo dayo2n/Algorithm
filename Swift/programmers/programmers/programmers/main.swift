89874863a19e31220731dcdba4040e1f3bbc0e50
dayo2n <0217dayun@naver.com>
2022-09-29 23:17:28 +0900

    [remove] make folder for JAVA

--- Swift/programmers/programmers/programmers/main.swift
+++ Swift/programmers/programmers/programmers/main.swift
@@ -1,12 +1,17 @@
-//
-//  main.swift
-//  Programmers
-//
-//  Created by 문다 on 2022/05/21.
-//
-
 import Foundation
-func solution(_ s:String) -> String {
-    let numbers = s.split(separator: " ").map( { return Int($0)!} )
-    return "\(numbers.min()!) \(numbers.max()!)"
+
+// stages.count : 사용자 수
+// N : 스테이지 수
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var failure: [Int: Double] = [:]
+    for n in (0..<N) {
+        var user = Double(stages.filter{$0>n}.count) // 도전한 유저
+        var challenger = Double(stages.filter{$0==(n+1)}.count) // 아직 클리어하지 못한 유저
+        
+        if user == 0 {  failure[n] = 0.0 }
+        else { failure[n] = challenger / user }
+    }
+    print(failure.sorted{return $0.value<$1.value})
+    return []
 }
