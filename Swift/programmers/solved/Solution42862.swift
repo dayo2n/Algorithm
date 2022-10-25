084b92a2ac85ab39f8a241f3b8598d85a2aca0ec
dayo2n <0217dayun@naver.com>
2022-10-26 00:12:28 +0900

    [solve] Lv1. 체육복: 그리디

--- Swift/programmers/solved/Solution42862.swift
+++ Swift/programmers/solved/Solution42862.swift
@@ -1,1 +1,28 @@
+import Foundation
 
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    var curReserve: [Int] = reserve.sorted()
+    var curLost:[Int] = lost.sorted()
+    
+    for student in curLost {
+        if curReserve.contains(student) {
+            curReserve.remove(at: curReserve.index(of: student)!)
+            curLost.remove(at: curLost.index(of: student)!)
+        }
+    }
+    
+    var students: Int = n - curLost.count
+    for student in curLost {
+        if curReserve.contains(student) {
+            students += 1
+            curReserve.remove(at: curReserve.index(of: student)!)
+        } else if curReserve.contains(student - 1) {
+            students += 1
+            curReserve.remove(at: curReserve.index(of: student - 1)!)
+        } else if curReserve.contains(student + 1) {
+            students += 1
+            curReserve.remove(at: curReserve.index(of: student + 1)!)
+        }
+    }
+    return students
+}
