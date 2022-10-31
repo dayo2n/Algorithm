d7fc9f87cf915df64d4429798b2fc1b202a5f9d2
dayo2n <0217dayun@naver.com>
2022-10-31 23:14:50 +0900

    [refactor] Lv2. 위장: for문 대신 compactMap, filter, map, reduce 이용

--- Swift/programmers/solved/Solution42578.swift
+++ Swift/programmers/solved/Solution42578.swift
@@ -11,3 +11,14 @@ func solution(_ clothes:[[String]]) -> Int {
     }
     return categories.values.map{$0+1}.reduce(1,*) - 1
 }
+
+
+// 여러 함수를 이용 => 시간은 더 오래 걸린다 ..
+import Foundation
+
+func solution(_ clothes:[[String]]) -> Int {
+    var categories = Set(clothes.compactMap({$0.last})).map({ category in
+        return clothes.filter({ $0.last == category}).count + 1
+    })
+    return categories.reduce(1,*) - 1
+}
