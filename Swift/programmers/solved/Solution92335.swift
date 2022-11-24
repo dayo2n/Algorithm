4bc310b47bbcdd38e86eedac32f1a496c93c3a6c
dayo2n <0217dayun@naver.com>
2022-11-24 21:22:05 +0900

    [comment] 문제 해결법 작성

--- Swift/programmers/solved/Solution92335.swift
+++ Swift/programmers/solved/Solution92335.swift
@@ -6,7 +6,7 @@ func isPrime(_ n: Int) -> Bool {
     var divisor: Int = 2
     if n < divisor { return false }
     if n == divisor { return true }
-    while divisor <= Int(sqrt(Double(n))) {
+    while divisor <= Int(sqrt(Double(n))) { // 소수는 제곱근 n 내에서 확인 가능
         if n % divisor == 0 { return false }
         divisor += 1
     }
