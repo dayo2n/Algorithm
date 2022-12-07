080e68d9cc0e994b6da05b4e999972fc06418c5c
dayo2n <0217dayun@naver.com>
2022-12-07 23:46:46 +0900

    [unsolve] Lv3. 숫자게임: 시간초과

---
+++ Swift/programmers/solved/Solution12987.swift
@@ -0,0 +1,26 @@
+
+// [출처: 프로그래머스] https://school.programmers.co.kr/learn/courses/30/lessons/12987
+
+
+// 효율성 테스트에서 시간초과가 난 코드
+import Foundation
+
+func solution(_ a:[Int], _ b:[Int]) -> Int {
+    var sortedA: [Int] = a.sorted(by: >)
+    var sortedB: [Int] = b.sorted(by: <)
+    var score: Int = 0
+    
+    while !sortedA.isEmpty {
+        let itemA = sortedA.removeLast()
+        var itemB: Int?
+        for (index, value) in sortedB.enumerated() {
+            if value > itemA {
+                itemB = sortedB.remove(at: index)
+                break
+            }
+        }
+        if let _ = itemB { score += 1 }
+        else { sortedB.removeFirst() }
+    }
+    return score
+}
