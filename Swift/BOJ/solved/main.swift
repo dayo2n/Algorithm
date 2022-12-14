71b21cca67538be658ea16b08da8cf90c2378950
dayo2n <0217dayun@naver.com>
2022-12-14 22:29:11 +0900

    [template] clear main.swift

--- Swift/BOJ/solved/main.swift
+++ Swift/BOJ/solved/main.swift
@@ -1,15 +1,1 @@
-//
-//  main.swift
-//  forBOJ
-//
-//  Created by 문다 on 2022/01/13.
-//
 
-//import Foundation
-
-print("         ,r'\"7")
-print("r`-_   ,'  ,/")
-print(" \\. \". L_r'")
-print("   `~\\/")
-print("      |")
-print("      |")
