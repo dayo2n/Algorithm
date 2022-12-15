1dba84706c30431a9079bae7f68c0d59d7d1640b
dayo2n <0217dayun@naver.com>
2022-12-15 23:25:53 +0900

    [solve] S4. 나는야 포켓몬 마스터 이다솜

---
+++ Swift/BOJ/solved/1620.swift
@@ -0,0 +1,24 @@
+
+import Foundation
+
+let input: [String] = readLine()!.split(separator: " ").map{ String($0) }
+let N: Int = Int(input[0])!
+let M: Int = Int(input[1])!
+
+var bookByName: [String: Int] = [:]
+var bookByIndex: [String] = []
+
+for _ in 0..<N {
+    let pocketmon: String = readLine()!
+    bookByName[pocketmon] = bookByIndex.count
+    bookByIndex.append(pocketmon)
+}
+
+for _ in 0..<M {
+    let quiz: String = readLine()!
+    
+    // 이름에는 영문만 포함하므로 첫 글자가 숫자면 입력값이 숫자인 것
+    if quiz[quiz.startIndex].isNumber { print(bookByIndex[Int(quiz)!-1]) }
+    else { print(bookByName[quiz]! + 1) }
+}
+
