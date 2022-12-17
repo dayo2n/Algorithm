2ef50613f645e8bcf30077fd3308d5419eb7353f
dayo2n <0217dayun@naver.com>
2022-12-18 02:21:41 +0900

    [solve] S4. 게임을 만든 동준이: 그리디

--- Swift/BOJ/solved/2847.swift
+++ Swift/BOJ/solved/2847.swift
@@ -1,3 +1,24 @@
 
 // [출처: BOJ] https://www.acmicpc.net/problem/2847
 
+import Foundation
+
+let N: Int = Int(readLine()!)!
+var scores: [Int] = []
+
+for _ in 0..<N {
+    scores.append(Int(readLine()!)!)
+}
+
+var reduced: Int = 0
+for (index, score) in scores.reversed().enumerated() {
+    if index == 0 { continue }
+    
+    if score >= scores[scores.count - index] {
+        let reduce: Int = score - scores[scores.count - index] + 1
+        reduced += reduce
+        scores[scores.count - index - 1] -= reduce
+    }
+}
+
+print(reduced)
