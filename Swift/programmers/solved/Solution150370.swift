910d456f32deb69eb4ceaf02ad6ed7e46ab7b404
dayo2n <0217dayun@naver.com>
2023-01-12 01:06:06 +0900

    [solve] Lv1. 개인정보 수집 유효기간(2): 날짜를 일수로 바꾸어 계산

--- Swift/programmers/solved/Solution150370.swift
+++ Swift/programmers/solved/Solution150370.swift
@@ -2,7 +2,6 @@
 // [출처: 프로그래머스] https://school.programmers.co.kr/learn/courses/30/lessons/150370
 
 // 1안. 문자열 파싱 후 비교
-
 import Foundation
 
 func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
@@ -62,4 +61,34 @@ func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
 }
 
 
-// 2안. 날짜를 모두 초로 바꾸어 계산
+// 2안. 날짜를 모두 1일 기준으로 바꾸어 계산
+import Foundation
+
+func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
+    
+    var expired: [Int] = []
+    
+    let splitToday: [Int] = today.split(separator: ".").map({Int(String($0))!})
+    let todaySum: Int = splitToday[0] * 12 * 28 + splitToday[1] * 28 + splitToday[2]
+     
+    var termInDictionary: [String: Int] = [:]
+    terms.forEach { term in
+                   let splitTerm: [String] = term.split(separator: " ").map({String($0)})
+                   termInDictionary[splitTerm[0]] = Int(splitTerm[1])!
+                  }
+    
+    for (index, date) in privacies.enumerated() {
+        let privacyType: String = String(date.split(separator: " ")[1])
+        let parse: [Int] = date.split(separator: " ")[0].split(separator: ".").map({Int(String($0))!})
+        var (year, month, day) = (parse[0], parse[1], parse[2]) // year, month, day
+        
+        let sum: Int = year * 12 * 28 + month * 28 + day + termInDictionary[privacyType]! * 28
+
+        // compare
+        if sum <= todaySum {
+            expired.append(index+1)
+        }
+    }
+    
+    return expired
+}
