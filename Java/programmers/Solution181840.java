7108ab6eca4f5b4d70b000f18f89aaa3553346e8
Dayeon Moon <0217dayun@naver.com>
2024-04-10 00:57:23 +0900

    [solve] Lv0. 정수 찾기: foreach

---
+++ Java/programmers/Solution181840.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] num_list, int n) {
+        for (int number: num_list) {
+            if (number == n) { 
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
