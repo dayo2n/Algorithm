aede1d0aec73d58ecbc8da2f0e461c04fb882dce
Dayeon Moon <0217dayun@naver.com>
2024-04-14 02:04:03 +0900

    [solve] G5. 전깃줄: DP(LIS)

--- Swift/BOJ/2565.swift
+++ Swift/BOJ/2565.swift
@@ -1,3 +1,13 @@
+
+// [출처: BOJ] https://www.acmicpc.net/problem/2565
+/*
+ DP긴한데 문제 해결 방법을 잘 캐치해서 LIS(가장 긴 증가하는 수열) 알고리즘을 이용하는 것이었음
+ 한 쪽 값으로 정렬 후 다른 쪽 값에서 가장 긴 증가하는 수열의 개수를 찾아 총 개수에서 빼야 함
+ 꼬인 것이 여러 개 있을 때 뭘 먼저 빼는지에 따라 답이 달라질 수 있음
+ 때문에 가장 긴 증가하는 수열을 찾아야 하는 거
+ 아래 링크 참조
+ https://www.acmicpc.net/board/view/84972
+ */
 struct Pair {
     let left: Int
     let right: Int
@@ -29,3 +39,4 @@ for (index, cable) in pairs.enumerated() {
 if let max = dp.max() {
     print(max == 1 ? 0 : numberOfCable - dp.max()!)
 }
+
