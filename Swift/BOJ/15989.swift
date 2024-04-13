836d0267feab07e7d69593108bf35cab0ed6a057
Dayeon Moon <0217dayun@naver.com>
2024-04-14 04:04:15 +0900

    [solve] G5. 1, 2, 3 더하기 4: 다이나믹프로그래밍

--- Swift/BOJ/15989.swift
+++ Swift/BOJ/15989.swift
@@ -1,3 +1,14 @@
+
+// [출처: BOJ] https://www.acmicpc.net/problem/15989
+/*
+ ㄹㅇ 이런건 누가 다들 어케 푸는거냐...
+ 점화식 찾기 어렵따...
+ 근접하게 접근은 했는데 까빙
+ 방식은 여러 개 일수도 있는듯...
+ 아래 링크 참고
+ https://velog.io/@lchyung1998/어려워요알고리즘-BOJ-15989-1-2-3-더하기-4
+ */
+
 let testcase = Int(readLine()!)!
 
 for _ in 0..<testcase {
