ed59e614bf5c33cb17379c0b7a732d38257433de
Dayeon Moon <0217dayun@naver.com>
2024-04-15 22:46:59 +0900

    [solve] S1. 오르막 수: dp

---
+++ Swift/BOJ/11057.swift
@@ -0,0 +1,25 @@
+
+// [출처: BOJ] https://www.acmicpc.net/problem/11057
+/*
+ DP
+ */
+
+let N = Int(readLine()!)!
+
+if N == 1 {
+    print(10)
+    exit(0)
+}
+var dp = [[Int]]()
+dp.append(Array(repeating: 0, count: 10))
+dp.append(Array(repeating: 1, count: 10))
+
+for length in 2...N {
+    var array = Array(repeating: 0, count: 10)
+    for number in 0..<10 {
+        array[number] = dp[length - 1][number..<10].reduce(0, +) % 10_007
+    }
+    dp.append(array)
+}
+
+print(dp[N].reduce(0, +) % 10_007)
