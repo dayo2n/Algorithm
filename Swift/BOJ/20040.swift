b6b1971974724dfee0abd7be2ee632c06afde63e
Dayeon Moon <0217dayun@naver.com>
2024-04-17 00:26:50 +0900

    [solve] G4. 사이클게임: union & find

---
+++ Swift/BOJ/20040.swift
@@ -0,0 +1,45 @@
+
+// [출처: BOJ] https://www.acmicpc.net/problem/20040
+/*
+분리집합 union&find
+ */
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let totalDot = input[0]
+let totalRound = input[1]
+var parent = [Int](0..<totalDot)
+
+func findRoot(_ child: Int) -> Int {
+    if parent[child] == child {
+        return child
+    }
+    
+    parent[child] = findRoot(parent[child])
+    return parent[child]
+}
+
+func union(_ first: Int, _ second: Int) {
+    let parentFirst = findRoot(first), parentSecond = findRoot(second)
+    
+    if parentFirst < parentSecond {
+        parent[parentSecond] = parentFirst
+    } else {
+        parent[parentFirst] = parentSecond
+    }
+}
+
+for round in 0..<totalRound {
+    let dots = readLine()!.split(separator: " ").map { Int($0)! }
+    let first = dots[0], second = dots[1]
+    
+    if findRoot(first) == findRoot(second) {
+        print(round + 1)
+        break
+    }
+    
+    union(first, second)
+    
+    if round == totalRound - 1 {
+        print(0)
+    }
+}
