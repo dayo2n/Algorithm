adbba7dffb1a5c768ed5495ff5aabdfcfef7b636
Dayeon Moon <0217dayun@naver.com>
2024-04-17 20:20:05 +0900

    [unsolve] G2. 공항: 분리집합

--- Swift/BOJ/10775.swift
+++ Swift/BOJ/10775.swift
@@ -1,3 +1,13 @@
+
+// [출처: BOJ] https://www.acmicpc.net/problem/10775
+/*
+ 아니 이거 분리집합이래서 풀었는데 문제도 이해못했음
+ 비행기-게이트간 도킹 시스템이 뭐 어떻게 되는데 이렇게 배치하는거야..?
+ 모름
+ 이거 푼거아님 ㄹㅇ
+ 더 강해져서 다시 풀러와라
+ */
+
 let totalGate = Int(readLine()!)!
 let totalPlane = Int(readLine()!)!
 var parent = [Int](0...totalGate)
