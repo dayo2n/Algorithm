4d06f5df4db9136126ea350ae9f9e68c1d16f0bd
Dayeon Moon <0217dayun@naver.com>
2024-04-19 14:48:00 +0900

    [solve] S3. 게임: 이분탐색

--- Swift/BOJ/1072.swift
+++ Swift/BOJ/1072.swift
@@ -1,3 +1,10 @@
+
+// [출처: BOJ] https://www.acmicpc.net/problem/1072
+/*
+ 왜인지 모르겠지만 50일 때도 -1이 나온다고 생각해서 여러 번 틀림
+ 그리고 부동소수점때문에, 연산 순서에 따라 100 * (Y / X)랑 100 * Y / X의 계산값이 다름
+ */
+
 import Foundation
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let (X, Y, Z) = (input[0], input[1], getAverage(input[1], input[0]))
@@ -23,8 +30,3 @@ print(minRound)
 func getAverage(_ top: Int, _ bottom: Int) -> Int {
     Int(100.0 * Double(top) / Double(bottom))
 }
-
-/*
- 6904434 3986253
- 43617
- */
