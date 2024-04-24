0510d64af65bb86897212fcc0994c730650b48a3
Dayeon Moon <0217dayun@naver.com>
2024-04-24 23:05:24 +0900

    [solve] Lv1. 평균 일일 대여 요금 구하기: SELECT, ROUND, AVG

--- Algorithm/Combination.swift
+++ Algorithm/Combination.swift
@@ -1,16 +1,28 @@
 
 /*
  Combination 조합
- 순서에 상관없게 나열하는 것
+ 서로 다른 n개의 원소에서 r개를 중복과 순서에 상관없게 나열하는 것
  */
 
-private func combination<T: Comparable>(_ targetArray: [T], _ targetNumber: Int, _ index: Int, _ currentArray: [T]) {
-    if currentArray.count == targetNumber {
+private func combination<T: Comparable>(_ targetArray: [T], _ targetCount: Int, _ index: Int, _ currentArray: [T]) {
+    if currentArray.count == targetCount {
         print(currentArray)
         return
     }
     
     for index in index..<targetArray.count {
         combination(targetArray, targetNumber, index + 1, currentArray + [targetArray[index]])
+    }
+}
+
+
+private func combination(targetArray: [Int], targetCount: Int, currentArray: [Int]) {
+    if currentArray.count == currentArray {
+        print(currentArray)
+        return
+    }
+    
+    for index in currentArray.count..<targetArray.count {
+        combination(targetArray: targetArray, targetCount: targetCount, currentArray: currentArray + [targetArray[index]])
     }
 }
