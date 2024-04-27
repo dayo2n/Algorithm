5a3694aabcaeab76ddb784bf2e52fe9ebbd7ea2d
Dayeon Moon <0217dayun@naver.com>
2024-04-28 00:02:10 +0900

    [solve] Lv3. 조건별로 분류하여 주문상태 출력하기: CASE WHEN THEN ELSE THEN END AS

---
+++ SQL/131113.sql
@@ -0,0 +1,9 @@
+SELECT ORDER_ID, PRODUCT_ID, TO_CHAR(OUT_DATE, 'YYYY-MM-DD'),
+       CASE WHEN TO_CHAR(OUT_DATE, 'YYYYMMDD') <= '20220501'
+            THEN '출고완료'
+            WHEN OUT_DATE IS NULL
+            THEN '출고미정'
+            ELSE '출고대기'
+       END AS STATUS
+FROM FOOD_ORDER
+ORDER BY ORDER_ID
